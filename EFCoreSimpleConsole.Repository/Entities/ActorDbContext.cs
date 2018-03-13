f57e0fe27261a12e325b40d94144f5913a4e719d
Erik Butcher <butcer0@gmail.com>
2018-03-13 14:55:21 -0400

    Add project files.

---
+++ EFCoreSimpleConsole.Repository/Entities/ActorDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSimpleConsole.Repository.Entities
+{
+    public class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO: Erik - 3/13/2018 Move to config file with encrypted connection string
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=ActorDb;"
+                + "Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EFCoreSimpleConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using EFCoreSimpleConsole.Repository.Entities;
+using System;
+using System.Linq;
+
+namespace EFCoreSimpleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ActorDbContext())
+            {
+                if(!db.Actors.Where(c => c.Name.Equals("Bruce Lee", StringComparison.InvariantCultureIgnoreCase)).Any()
+                    && !db.Actors.Where(c => c.Name.Equals("Madonna", StringComparison.InvariantCultureIgnoreCase)).Any())
+                {
+                    Program.SeedData(db);
+                }
+
+                var count = db.SaveChanges();
+
+                // Fetch data & write it out
+                Console.WriteLine($"{count} records added");
+                foreach (var Actor in db.Actors)
+                {
+                    Console.WriteLine($"Name - {Actor.Name},\t" +
+                                      $"Age: {Actor.Age},\t" +
+                                      $"Academy Winner: {Actor.AcademyWinner}");
+                }
+                Console.ReadLine();
+            }
+        }
+
+        internal static void SeedData(ActorDbContext db)
+        {
+            // Seed
+            db.Actors.AddRange(new Actor
+            {
+                Name = "Bruce Lee",
+                Age = 75,
+                AcademyWinner = false
+            },
+            new Actor
+            {
+                Name = "Madonna",
+                Age = 55,
+                AcademyWinner = true
+            });
+        }
+    }
+}
