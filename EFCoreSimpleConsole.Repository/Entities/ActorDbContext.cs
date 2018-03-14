0dd6d5bea3a3a652acbe9fa8d333b03ec8fceb55
Erik Butcher <butcer0@gmail.com>
2018-03-14 16:47:09 -0400

    Updated to use remote connection to sql server 2017 on ubuntu 16.04 Server03

--- EFCoreSimpleConsole.Repository/Entities/ActorDbContext.cs
+++ EFCoreSimpleConsole.Repository/Entities/ActorDbContext.cs
@@ -9,9 +9,14 @@ namespace EFCoreSimpleConsole.Repository.Entities
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //TODO: Erik - 3/13/2018 Move to config file with encrypted connection string
-            optionsBuilder.UseSqlServer(
-                @"Server=(localdb)\mssqllocaldb;Database=ActorDb;"
-                + "Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(
+            //    @"Server=(localdb)\mssqllocaldb;Database=ActorDb;"
+            //    + "Trusted_Connection=True;");
+
+            #region Depricated - Use Linux SQL Server DB
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ActorDb;Trusted_Connection=True;");
+            #endregion
+            optionsBuilder.UseSqlServer(@"Server=108.161.137.144; Initial Catalog = master; Database=ActorDb; Integrated Security = False; User ID = SA; Password = Password1; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
         }
     }
 }
